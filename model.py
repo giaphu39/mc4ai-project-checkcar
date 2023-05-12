97feb3c833fbee4d871198191b6bed3dc0a54960
Amalkin <trangiaphu39@gmail.com>
2023-05-12 20:38:35 +0700

    ver 3.2 (completed)

--- model.py
+++ model.py
@@ -4,11 +4,12 @@ import torchvision.transforms as transforms
 import pickle
 import numpy as np
 from PIL import Image
-from tqdm import tqdm
 import cv2
+from tqdm import tqdm
+import pygame
+import pygame.mixer as mixer
 import pygame
 from sklearn.linear_model import LogisticRegression
-
 class PretrainModel:
     def __init__(self):
         self.model = torch.load("models/pretrained.pkl")
@@ -54,42 +55,50 @@ def load_model():
     with open("model.pkl", "rb") as f:
         model = pickle.load(f)
     return model
+def main():
+    pretrained_model = PretrainModel()
+    label_dict = read_training_data_label()
+    data_dict = read_training_data()
+    X = []
+    y = []
+    for filename, features in data_dict.items():
+        label = label_dict.get(filename)
+        if label is not None:
+            X.append(features)
+            y.append(label)
 
-pretrained_model = PretrainModel()
-label_dict = read_training_data_label()
-data_dict = read_training_data()
-X = []
-y = []
-for filename, features in data_dict.items():
-    label = label_dict.get(filename)
-    if label is not None:
-        X.append(features)
-        y.append(label)
-
-model = train_model(X, y)
-save_model(model)
-loaded_model = load_model()
-pygame.init()
-audio_file = "warning.mp3"
-pygame.mixer.music.load(audio_file)
-cap = cv2.VideoCapture(0)
-dem = 0
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    if ret:
-        input_image = frame
-        input_image = Image.fromarray(input_image)
-        feature_vector = pretrained_model.get_feature(input_image)
-        prediction = loaded_model.predict([feature_vector])
-        if prediction == 0: dem = 0
-        if prediction == 1:
-            print('warning')
-            if dem == 3000 or dem == 0:
-                pygame.mixer.music.play()
-                dem = 0
-            dem += 1
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    model = train_model(X, y)
+    save_model(model)
+    loaded_model = load_model()
+    mixer.init()
+    mixer.music.load(r"warning.mp3")
+    mixer.music.set_volume(0.7)
+    webcam=cv2.VideoCapture(0)
+    while True:
+        ret,frame=webcam.read()
+        if ret==True:
+            input_image = Image.fromarray(frame)
+            feature_vector = pretrained_model.get_feature(input_image)
+            prediction = loaded_model.predict([feature_vector])
+            height, width = frame.shape[:2]
+            color = (0, 255, 0) 
+            x1 = width // 2 - 200
+            y1 = height // 2 - 200
+            x2 = width // 2 + 200
+            y2 = height // 2 + 200
+            if prediction == 1:
+                color = (0, 0, 255) 
+                if not mixer.music.get_busy():
+                    mixer.music.play(fade_ms=3000)
+                    print("detected car")
+            else:
+                color = (0, 255, 0)
+            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        cv2.imshow("cars",frame)
+        key=cv2.waitKey(60) & 0xFF
+        if prediction == 1 and (not mixer.music.get_busy()):
+            mixer.music.play(fade_ms=3000)
+            print("detected car")
+        if key==ord("q"):
+            break
+main()
